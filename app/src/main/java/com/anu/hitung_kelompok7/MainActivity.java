16d6ca0d40fdd52e3215c36b4b438c058de4ae38
arpo1 <w>
2022-04-06 15:05:40 +0700

    barV2

--- app/src/main/java/com/anu/hitung_kelompok7/MainActivity.java
+++ app/src/main/java/com/anu/hitung_kelompok7/MainActivity.java
@@ -1,14 +1,31 @@
 package com.anu.hitung_kelompok7;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.anu.hitung_kelompok7.R;
 import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager =  findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        VPadapter vPadapter = new VPadapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vPadapter.addFragmen(new Fragment1(), "Home");
+
+
+
+
     }
 }
